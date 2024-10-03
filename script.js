9a436f64da15717d6039b7eb8df6052f0c2f8a9b
Marianne Anderson <mganderson13@gmail.com>
2024-10-03 16:17:10 -0500

    created html and js files. wrote basic function logic

---
+++ script.js
@@ -0,0 +1,33 @@
+let a = "";
+let b = "";
+let operator = "";
+
+function operate(a, b, operator) {
+
+// create if statements that will complete one of the functions depending on operator
+    if (operator === "add") {
+
+    }
+
+}
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        return "ERR";
+    } else {
+    return a/b;
+    }
+}
